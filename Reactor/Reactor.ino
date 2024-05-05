33e58cd27e1fd7faaa5b4a26f82cf0ca99314de9
NodCaleb <nod.caleb@gmail.com>
2024-05-05 09:09:47 +0300

    Ractor blue circle

--- Reactor/Reactor.ino
+++ Reactor/Reactor.ino
@@ -1,7 +1,10 @@
 #include <FastLED.h>
-#define NUM_LEDS 3
+#define NUM_LEDS 22
+#define STRIP_WIDTH 11
 #define LED_PIN 10
+#define DELAY_TIME 10
 CRGB leds[NUM_LEDS];
+int step = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,7 +14,12 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   for (int i = 0; i < NUM_LEDS; i++){
-    leds[i] = CHSV(164, 255, 255);
+    if (i >= step && i < step + STRIP_WIDTH || step > NUM_LEDS - STRIP_WIDTH && (i >= step || i < step + STRIP_WIDTH - NUM_LEDS)) leds[i] = CHSV(164, 255, 255);
+    else leds[i] = CHSV(164, 255, 0);
   }
   FastLED.show();
+  
+  step++;
+  if (step == NUM_LEDS) step = 0;
+  delay(DELAY_TIME);
 }
